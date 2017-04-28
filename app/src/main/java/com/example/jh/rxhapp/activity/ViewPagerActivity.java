6fc8494de3a3f72c69fee5ca9259eccac352042a
renxh3 <renxh3@lenovo.com>
2017-04-28 15:34:46 +0800

    frist

---
+++ app/src/main/java/com/example/jh/rxhapp/activity/ViewPagerActivity.java
@@ -0,0 +1,15 @@
+package com.example.jh.rxhapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jh.rxhapp.R;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+    }
+}
